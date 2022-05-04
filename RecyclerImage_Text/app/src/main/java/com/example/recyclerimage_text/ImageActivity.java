927a6a5d750b85d892a0054ceccae142b45a23c6
NoTitec <zkxkr0@gmail.com>
2022-05-04 15:55:59 +0900

    0504

---
+++ RecyclerImage_Text/app/src/main/java/com/example/recyclerimage_text/ImageActivity.java
@@ -0,0 +1,27 @@
+package com.example.recyclerimage_text;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ImageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.image_main);
+
+        ImageView imgView= findViewById(R.id.imgView);
+        //prepare for receiving intent from adapter
+        Intent intent=new Intent(this.getIntent());//intent가 있는지 확인하고 가져옴
+        int s = intent.getIntExtra("image", 0);//값이없을경우 default는 0으로
+        Drawable dr = getDrawable(s);
+        imgView.setImageDrawable(dr);
+        //this data is just for test~
+        String as = intent.getStringExtra("mydata");
+        Log.e("***", "Received data is " + as); //check data
+    }
+}
--- RecyclerImage_Text/app/src/main/java/com/example/recyclerimage_text/WordListAdapter.java
+++ RecyclerImage_Text/app/src/main/java/com/example/recyclerimage_text/WordListAdapter.java
@@ -1,7 +1,10 @@
 package com.example.recyclerimage_text;
 
+import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +14,8 @@ import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.List;
 
 public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.MyViewHolder> {
@@ -109,19 +114,43 @@ class MyViewHolder extends RecyclerView.ViewHolder
 
     public void onClick(View view) {
 
-        String ss="";
-
-        if( view.getId() == R.id.txtName) ss = "name";
-        if( view.getId()==R.id.txtKName) ss="kname";
-        if( view.getId() == R.id.imgName) ss = "image ";
+//        String ss="";
+//
+//        if( view.getId() == R.id.txtName) ss = "name";
+//        if( view.getId()==R.id.txtKName) ss="kname";
+//        if( view.getId() == R.id.imgName) ss = "image ";
 
         // Get the position of the item that was clicked.
 
         int mPosition = getLayoutPosition();
 
-        Toast.makeText(mContext,ss + " Position = "+mPosition+"\n Item = "
+        switch ( view.getId() ) {
+            case R.id.txtName: case R.id.txtKName: // ss = "name";
+                // search method 1
+                Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);//
+                intent.putExtra(SearchManager.QUERY, itemsList.get(mPosition).getTitleStr()); // query contains search string
+                // search method 2
+//                try {
+//                    String eQuery = URLEncoder.encode(itemsList.get(mPosition).getTitleStr(), "UTF-8");
+//                    Uri uri = Uri.parse("http://www.google.com/search?q=" + eQuery);
+//                    intent = new Intent(Intent.ACTION_VIEW, uri);//intent타입과 값제공
+//                } catch (UnsupportedEncodingException e) {
+//                    e.printStackTrace();
+//                }
+                mContext.startActivity(intent);
+                break;
 
-                +itemsList.get(mPosition).getTitleStr(),Toast.LENGTH_LONG).show();
+            case R.id.imgName: //) ss = "image ";
+                Intent sintent=new Intent(mContext,ImageActivity.class);//메인 activity context를 imageactivity로 보내겠다(main과 어댑터는 같은 context다)
+                sintent.putExtra("image", itemsList.get(mPosition).getImgID());//intent객체에 전달한 자료
+                sintent.putExtra("mydata", "if you need more");
+
+                mContext.startActivity(sintent);//제어를 ImageActivity로 넘김
+                break;
+        }
+//        Toast.makeText(mContext,ss + " Position = "+mPosition+"\n Item = "
+//
+//                +itemsList.get(mPosition).getTitleStr(),Toast.LENGTH_SHORT).show();
 
     }
 
